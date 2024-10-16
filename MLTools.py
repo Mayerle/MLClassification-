6f4af49bcc89eaa229a4d3e3735c756bcdfb934c
Ivan Mayerle <ivanmayerle@yandex.ru>
2024-10-16 09:46:21 +0700

    2D lene regression

--- MLTools.py
+++ MLTools.py
@@ -1,21 +1,42 @@
-
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import math
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from scipy.optimize import minimize, rosen, rosen_der,curve_fit, minimize
+from MLTools import *       
+
+def get_zero_array(n: int) -> list:
+    return [0 for x in range(n)]
+
+def find_classification_line(X,Y,C):
+    nx,ny,b =  minimize(min_function2D_MSE, x0 = (0,0,0),args=(X,Y,C), method='trust-constr', tol=1e-10).x
+    return (nx,ny,b)
 
 
+def classify_value(value, mark) -> int:
+    if(value == mark):
+        return 1
+    else:
+        return -1
 #plane: (n,x) = 0
 def plane_projection2D(x,y,nx,ny):
     return np.dot((nx,ny),(x,y))
 
-def classifier2D(x,y,nx,ny):
-    return np.sign(plane_projection2D(x,y,nx,ny))
+def classifier2D(x,y,nx,ny,b):
+    return np.sign(plane_projection2D(x,y-b,nx,ny))
 
-def min_function2D(params,X,Y,C):
+
+def get_zero_array(n: int) -> list:
+    return [0 for x in range(n)]
+
+
+def min_function2D_MSE(params,X,Y,C):
     
     def l_MSE(nx,ny,b, x,y,c):
         return (1 - c*plane_projection2D(x, y-b, nx, ny))**2
+    #def l_MSE(nx,ny,b, x,y,c):
+    #    return (1 - c*plane_projection2D(x, y-b, nx, ny))**2
     
     nx,ny,b = params
     summ = 0
@@ -23,8 +44,42 @@ def min_function2D(params,X,Y,C):
         summ += l_MSE(nx,ny,b, X[i],Y[i], C[i])
     return summ
 
+def calculate_accuracy(objects,targets,classifier):
+    count = len(targets)
+    corrects = 0
+    i=0
+    for vector in objects:
+        if(classifier(vector) == targets[i]):
+            corrects+=1
+        i+=1
+    return corrects/count
+    
+def calculate_precision_recall(objects,targets,classifier):
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+    i  = 0
+    for vector in objects:
+        prediction = classifier(vector)
+        target = targets[i]
+        if(prediction == target):
+            if(prediction == 1):
+                tp+=1
+            else:
+                tn+=1
+        else:
+            if(prediction == 1):
+                fp+=1
+            else:
+                fn+=1
+        i+=1
+    presicion = tp/(tp+fp)
+    recall = tp/(tp+fn)
+    #return (tp,fp,tn,fn)
+    return (presicion,recall)
 
-def plot_classes2D(ax,X,Y,C,nx,ny,b,color):
+def plot_classes2D(ax,nx,ny,b,color,X,Y,C):
     x = []
     y = []
     for i in range(len(C)):
--- main.py
+++ main.py
@@ -6,58 +6,117 @@ from matplotlib.colors import ListedColormap
 from scipy.optimize import minimize, rosen, rosen_der,curve_fit, minimize
 from MLTools import *       
 
+def sample_data(data):
+    x = list(data.iloc[:,0])
+    y = list(data.iloc[:,1])
+    c = list(data.iloc[:,2])
+    return (x,y,c)
+#classes = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"] #Classes
+#["SepalLengthCm","SepalWidthCm","PetalLengthCm","PetalWidthCm","Class"]
 
 
 data_sheet = pd.read_csv("iris.csv")
-classes = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"] #Species
 
-def classify_value(value: str, mark: str) -> int:
-    if(value == mark):
-        return 1
-    else:
-        return -1
-    
-def dataset_classificator(s):
-    if(s == "Iris-setosa"):
-        return 1
-    else:
-        return -1
-    
-
-data = data_sheet[["SepalLengthCm","SepalWidthCm","PetalLengthCm","PetalWidthCm","Species"]]
-
-data_setosa                = data.copy()
-data_setosa["Species"]     = data_setosa["Species"].map(lambda x: classify_value(x,"Iris-setosa"))
-data_versicolor            = data.copy()
-data_versicolor["Species"] = data_versicolor["Species"].map(lambda x: classify_value(x,"Iris-versicolor"))
-data_virginica             = data.copy()
-data_virginica["Species"]  = data_virginica["Species"].map(lambda x: classify_value(x,"Iris-virginica"))
+data_frame = data_sheet.rename({"Species": "Class"}, axis='columns')
+data_frame = data_frame[["SepalLengthCm","SepalWidthCm","Class"]]
 
 
-def data_sampler(n,k,data):
-    ss = ["SepalLengthCm","SepalWidthCm","PetalLengthCm","PetalWidthCm"]
-    s_x = ss[n]
-    s_y = ss[k]
-    s_c = "Species"
-    x = list(data[s_x])
-    y = list(data[s_y])
-    c = list(data[s_c])
-    return (x,y,c)
+df_setosa              = data_frame.copy()
+df_versicolor          = data_frame.copy()
+df_virginica           = data_frame.copy()
 
-x_setosa    , y_setosa    , c_setosa     = data_sampler(0,1,data_setosa)
-x_versicolor, y_versicolor, c_versicolor = data_sampler(0,1,data_versicolor)
-x_virginica , y_virginica , c_virginica  = data_sampler(0,1,data_virginica)
+df_setosa["Class"]     = df_setosa["Class"].map(lambda x: classify_value(x,"Iris-setosa"))
+df_versicolor["Class"] = df_versicolor["Class"].map(lambda x: classify_value(x,"Iris-versicolor"))
+df_virginica["Class"]  = df_virginica["Class"].map(lambda x: classify_value(x,"Iris-virginica"))
+
+
+
+
+setosa     = sample_data(df_setosa)
+versicolor = sample_data(df_versicolor)
+virginica  = sample_data(df_virginica)
+
+
+
+accuracies = []
+precisions = []
+recalls = []
 
 fig, ax = plt.subplots()
-minimization_function0 = lambda func, args: minimize(func, x0 = (0,0,0),args=args, method='trust-constr', tol=1e-10).x
 
-def find_classification_line(ax,X,Y,C,color,minimization_function):
-    nx,ny,b = minimization_function(min_function2D,(X,Y,C))
-    plot_classes2D(ax,X,Y,C,nx,ny,b,color)
-    
-find_classification_line(ax,x_setosa,y_setosa,c_setosa,"red",minimization_function0)
-find_classification_line(ax,x_versicolor,y_versicolor,c_versicolor,"green",minimization_function0)
-find_classification_line(ax,x_virginica,y_virginica,c_virginica,"blue",minimization_function0)
+nx,ny,b = find_classification_line(*setosa)
+plot_classes2D(ax,nx,ny,b,"red",*setosa)
 
+
+classificator = lambda xy: classifier2D(xy[0],xy[1],nx,ny,b)
+x,y,targets = setosa
+objects = list(zip(x,y))
+accuracy = calculate_accuracy(objects,targets,classificator)
+precision, recall = calculate_precision_recall(objects,targets,classificator)
+accuracies.append(accuracy)
+precisions.append (precision)
+recalls.append (recall)
+
+
+nx,ny,b = find_classification_line(*versicolor)
+plot_classes2D(ax,nx,ny,b,"blue",*versicolor)
+
+classificator = lambda xy: classifier2D(xy[0],xy[1],nx,ny,b)
+x,y,targets = versicolor
+objects = list(zip(x,y))
+accuracy = calculate_accuracy(objects,targets,classificator)
+precision, recall = calculate_precision_recall(objects,targets,classificator)
+accuracies.append(accuracy)
+precisions.append (precision)
+recalls.append (recall)
+
+nx,ny,b = find_classification_line(*virginica)
+plot_classes2D(ax,nx,ny,b,"green",*virginica)  
+
+classificator = lambda xy: classifier2D(xy[0],xy[1],nx,ny,b)
+x,y,targets = virginica
+objects = list(zip(x,y))
+accuracy = calculate_accuracy(objects,targets,classificator)
+precision, recall = calculate_precision_recall(objects,targets,classificator)
+accuracies.append(accuracy)
+precisions.append (precision)
+recalls.append (recall)
+
+
+accuracy = (accuracies[0]+accuracies[1]+accuracies[2])/3
+precision = (precisions[0]+precisions[1]+precisions[2])/3
+recall = (recalls[0]+recalls[1]+recalls[2])/3
+
+print(f"{accuracy:.2f} | {precision:.2f} | {recall:.2f}")
 ax.grid()
+#plt.show()
+
+    
+    
+#ax = fig.add_subplot(projection='3d')
+ 
+ 
+markers_dict = {
+    "Iris-setosa" : "s", 
+    "Iris-versicolor" : "o",
+    "Iris-virginica" : "^"   
+    }
+
+classes = data_frame["Class"].unique()
+
+#for clss in classes:
+#    plot_data = data_frame[data_frame["Class"] == clss]
+ #   marker = markers_dict[clss]
+ #   x = plot_data.iloc[:,0]
+  #  y = plot_data.iloc[:,1]
+  #  z = plot_data.iloc[:,2]
+  #  ax.scatter(x,y,z)
+
+#xx = np.linspace(0,5,100)
+
+#X = np.arange(0, 160, 2)
+#Y = np.arange(0, 160, 2)
+#X, Y = np.meshgrid(X, Y)
+#Z = np.sqrt(X+Y)
+#ax.plot_surface(X, Y, Z, alpha=0.2)
 plt.show()
